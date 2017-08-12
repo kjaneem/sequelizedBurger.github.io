fb6ad72215903a0d23ca5b9153823cc69f1f5b0a
Karen Manley <Karen Manley>
2017-08-11 22:45:07 -0400

    update connection file

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "silver63",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "silver63",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port:  3306,
+    host: 'localhost',
+    user: 'root',
+    password: 'silver63',
+    database: 'burgers_db'
+  });
+};
 
 
 connection.connect(function(err) {
